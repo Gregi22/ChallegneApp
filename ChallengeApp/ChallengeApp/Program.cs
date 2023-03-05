9cd92adf5754673b68377d39aef49860c24cb39a
grzegorz_seklecki@wp.pl <grzegorz_seklecki@wp.pl>
2023-03-05 12:55:36 +0100

    Zadanie Domowe nr 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// liczby calkowite
+int myAge = 30;
+int newAge = myAge + 2;
+Console.WriteLine(newAge);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// liczby zmienno przecinkowe
+float myNumber = float.MaxValue;
+double myNuber2 = double.MaxValue;
+
+// zmienne tekstowe 
+string name = "Grzegorz";
+string surname = "Seklecki";
+string result = name + surname;
+Console.WriteLine(result);
+char myVar5 = "G";
+var result2 = name.ToArray("Grzgorz");
+Console.WriteLine(result2);
+Console.WriteLine(myNuber2);
+
+
